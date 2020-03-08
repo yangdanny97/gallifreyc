c01ea19bfff24903ad3989ffc1379342c4afe0ce
yangdanny97 <dzy4@cornell.edu>
2020-03-07 20:52:06 -0800

    update scheduler

--- compiler/src/gallifreyc/GallifreyScheduler.java
+++ compiler/src/gallifreyc/GallifreyScheduler.java
@@ -1,17 +1,11 @@
 package gallifreyc;
 
 import gallifreyc.visit.SharedTypeWrapper;
+import gallifreyc.translate.*;
 import polyglot.ast.NodeFactory;
 import polyglot.ext.jl7.JL7Scheduler;
-import polyglot.frontend.CyclicDependencyException;
-import polyglot.frontend.ExtensionInfo;
-import polyglot.frontend.JLExtensionInfo;
-import polyglot.frontend.JLScheduler;
-import polyglot.frontend.Job;
-import polyglot.frontend.goals.Goal;
-import polyglot.frontend.goals.TypeChecked;
-import polyglot.frontend.goals.Disambiguated;
-import polyglot.frontend.goals.VisitorGoal;
+import polyglot.frontend.*;
+import polyglot.frontend.goals.*;
 import polyglot.types.TypeSystem;
 import polyglot.util.InternalCompilerError;
 
@@ -24,43 +18,71 @@ public class GallifreyScheduler extends JL7Scheduler {
         super(extInfo);
     }
     
-    @Override
-    public Goal Disambiguated(Job job) {
-        TypeSystem ts = extInfo.typeSystem();
-        NodeFactory nf = extInfo.nodeFactory();
-        Goal g = Disambiguated.create(this, job, ts, nf);
-        try {
-            g.addPrerequisiteGoal(WrapSharedType(job), this);
-        } catch (CyclicDependencyException e) {
-            throw new InternalCompilerError(e);
-        }
-        return g;
-    }
-    
-    @Override
-    public Goal TypeChecked(Job job) {
-        TypeSystem ts = extInfo.typeSystem();
-        NodeFactory nf = extInfo.nodeFactory();
-        Goal g = TypeChecked.create(this, job, ts, nf);
-        try {
-            g.addPrerequisiteGoal(WrapSharedType(job), this);
-        } catch (CyclicDependencyException e) {
-            throw new InternalCompilerError(e);
-          }     
-        return g;
-    }
-    
-    public Goal WrapSharedType(Job job) {
-        ExtensionInfo extInfo = job.extensionInfo();
-        TypeSystem ts = extInfo.typeSystem();
-        NodeFactory nf = extInfo.nodeFactory();
-        Goal g = new VisitorGoal(job, new SharedTypeWrapper(job, ts, nf));
+//    @Override
+//    public Goal Disambiguated(Job job) {
+//        TypeSystem ts = extInfo.typeSystem();
+//        NodeFactory nf = extInfo.nodeFactory();
+//        Goal g = Disambiguated.create(this, job, ts, nf);
 //        try {
-//            g.addPrerequisiteGoal(Disambiguated(job), this);
+//            g.addPrerequisiteGoal(WrapSharedType(job), this);
 //        } catch (CyclicDependencyException e) {
 //            throw new InternalCompilerError(e);
 //        }
-//        g.addCorequisiteGoal(Disambiguated(job), this);
+//        return g;
+//    }
+//    
+//    @Override
+//    public Goal TypeChecked(Job job) {
+//        TypeSystem ts = extInfo.typeSystem();
+//        NodeFactory nf = extInfo.nodeFactory();
+//        Goal g = TypeChecked.create(this, job, ts, nf);
+//        try {
+//            g.addPrerequisiteGoal(WrapSharedType(job), this);
+//        } catch (CyclicDependencyException e) {
+//            throw new InternalCompilerError(e);
+//          }     
+//        return g;
+//    }
+    
+    public Goal RewriteAssignment(Job job) {
+        Goal g =
+                new VisitorGoal(job,
+                                new AssignmentRewriter(job,
+                                                   extInfo,
+                                                   extInfo.outputExtensionInfo()));
+        try {
+            g.addPrerequisiteGoal(Serialized(job), this);
+        }
+        catch (CyclicDependencyException e) {
+            throw new InternalCompilerError(e);
+        }
         return internGoal(g);
     }
+    
+//    public Goal WrapSharedType(Job job) {
+//        ExtensionInfo extInfo = job.extensionInfo();
+//        TypeSystem ts = extInfo.typeSystem();
+//        NodeFactory nf = extInfo.nodeFactory();
+//        Goal g = new VisitorGoal(job, new SharedTypeWrapper(job, ts, nf));
+////        try {
+////            g.addPrerequisiteGoal(Disambiguated(job), this);
+////        } catch (CyclicDependencyException e) {
+////            throw new InternalCompilerError(e);
+////        }
+////        g.addCorequisiteGoal(Disambiguated(job), this);
+//        return internGoal(g);
+//    }
+    
+    @Override
+    public Goal CodeGenerated(Job job) {
+        Goal g = super.CodeGenerated(job);
+        try {
+            g.addPrerequisiteGoal(RewriteAssignment(job), this);
+        }
+        catch (CyclicDependencyException e) {
+            throw new InternalCompilerError(e);
+        }
+        return internGoal(g);
+    }
+
 }
