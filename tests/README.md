ab65eb12ab08390b4d4b372a5be273e91d6b77ca
yangdanny97 <dzy4@cornell.edu>
2020-03-29 14:59:19 -0700

    debugging

---
+++ tests/README.md
@@ -0,0 +1,2 @@
+To Run Tests:
+cd into tests directory, run ./test.sh
--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-pth=`dirname $0`/../../polyglot-master/bin/pth
+pth=`dirname $0`/../../polyglot/bin/pth
 
 if [ ! -f "$pth" ]; then
   # TODO: make sure polyglot/bin (which is where pth lives) is in your path
   pth=pth
 fi
 
-$pth -classpath ../compiler/classes pthScript
+$pth -classpath ../compiler/classes ./pthScript
