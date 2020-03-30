559db3f33e541fd154e4f02a81d9354983ee84bf
yangdanny97 <dzy4@cornell.edu>
2020-03-30 11:00:28 -0700

    add some tc overrides

--- compiler/src/gallifreyc/extension/GallifreyExprExt.java
+++ compiler/src/gallifreyc/extension/GallifreyExprExt.java
@@ -1,19 +1,67 @@
 package gallifreyc.extension;
 
+import gallifreyc.ast.MoveRef;
+import gallifreyc.ast.SharedRef;
+import gallifreyc.ast.UniqueRef;
 import gallifreyc.types.RefQualifiedType;
 import gallifreyc.types.RefQualifiedType_c;
 import polyglot.ast.Expr;
+import polyglot.ast.ExprOps;
+import polyglot.ast.Lang;
 import polyglot.ast.Node;
+import polyglot.ast.TypeNode;
+import polyglot.types.Type;
 import polyglot.util.Position;
 import polyglot.util.SerialVersionUID;
 
 // extra operations for expressions
-public class GallifreyExprExt extends GallifreyExt {
+public class GallifreyExprExt extends GallifreyExt implements ExprOps {
     private static final long serialVersionUID = SerialVersionUID.generate();
-    private boolean isConst = false; //TODO
     
     @Override
     public Expr node() {
         return (Expr) super.node();
+    }
+
+    @Override
+    public boolean constantValueSet(Lang lang) {
+        return superLang().constantValueSet(node(), lang);
+    }
+
+    @Override
+    public boolean isConstant(Lang lang) {
+        return superLang().isConstant(node(), lang);
+    }
+
+    @Override
+    public Object constantValue(Lang lang) {
+        return superLang().constantValue(node(), lang);
+    }
+    
+    public boolean isMove() {
+    	Type t = node().type();
+        if (t != null && t instanceof RefQualifiedType) {
+            RefQualifiedType rt = (RefQualifiedType) t;
+            return rt.refQualification() instanceof MoveRef;
+        }
+        return false;
+    }
+    
+    public boolean isUnique() {
+    	Type t = node().type();
+        if (t != null && t instanceof RefQualifiedType) {
+            RefQualifiedType rt = (RefQualifiedType) t;
+            return rt.refQualification() instanceof UniqueRef;
+        }
+        return false;
+    }
+    
+    public boolean isShared() {
+    	Type t = node().type();
+        if (t != null && t instanceof RefQualifiedType) {
+            RefQualifiedType rt = (RefQualifiedType) t;
+            return rt.refQualification() instanceof SharedRef;
+        }
+        return false;
     }
 }
--- compiler/src/gallifreyc/extension/GallifreySourceFileExt.java
+++ compiler/src/gallifreyc/extension/GallifreySourceFileExt.java
@@ -1,8 +1,12 @@
 package gallifreyc.extension;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import gallifreyc.ast.RestrictionDecl;
 import gallifreyc.types.RefQualifiedType;
 import gallifreyc.types.RefQualifiedType_c;
 import polyglot.ast.CanonicalTypeNode;
@@ -13,17 +17,24 @@ import polyglot.ast.ClassDecl_c;
 import polyglot.ast.ClassMember;
 import polyglot.ast.Expr;
 import polyglot.ast.Id_c;
+import polyglot.ast.Import;
 import polyglot.ast.Node;
 import polyglot.ast.SourceFile;
 import polyglot.ast.TopLevelDecl;
 import polyglot.ast.TypeNode;
+import polyglot.ast.Import.Kind;
+import polyglot.ext.jl5.ast.JL5Import;
 import polyglot.types.Flags;
+import polyglot.types.Named;
 import polyglot.types.ReferenceType;
+import polyglot.types.SemanticException;
 import polyglot.types.Type;
+import polyglot.types.TypeSystem;
 import polyglot.util.Copy;
 import polyglot.util.ListUtil;
 import polyglot.util.Position;
 import polyglot.util.SerialVersionUID;
+import polyglot.visit.TypeChecker;
 
 // extra operations for source files
 public class GallifreySourceFileExt extends GallifreyExt {
@@ -32,5 +43,22 @@ public class GallifreySourceFileExt extends GallifreyExt {
     @Override
     public SourceFile node() {
         return (SourceFile) super.node();
+    }
+    
+    @Override
+    public Node typeCheck(TypeChecker tc) throws SemanticException {
+    	// remove restriction decls during TC
+    	SourceFile n = node();
+    	List<TopLevelDecl> cd = new ArrayList<>();
+    	List<RestrictionDecl> rd = new ArrayList<>();
+    	for (TopLevelDecl d : n.decls()) {
+    		if (d instanceof ClassDecl) cd.add(d);
+    		if (d instanceof RestrictionDecl) rd.add((RestrictionDecl) d);
+    	}
+    	for (RestrictionDecl r : rd) {
+    		lang().typeCheck(r, tc);
+    	}
+    	SourceFile supertc = n.decls(cd);
+    	return superLang().typeCheck(supertc, tc);
     }
 }
