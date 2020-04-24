4ba6baae9655725481cd166da685cb705634148a
yangdanny97 <dzy4@cornell.edu>
2020-04-24 16:59:57 -0700

    cleanup + reorg

---
+++ compiler/src/gallifreyc/extension/GallifreyConstructorDeclExt.java
@@ -0,0 +1,9 @@
+package gallifreyc.extension;
+
+import polyglot.util.SerialVersionUID;
+
+public class GallifreyConstructorDeclExt extends GallifreyProcedureDeclExt {
+	private static final long serialVersionUID = SerialVersionUID.generate();
+	
+	//TODO
+}
--- compiler/src/gallifreyc/extension/GallifreyMethodDeclExt.java
+++ compiler/src/gallifreyc/extension/GallifreyMethodDeclExt.java
@@ -1,31 +1,15 @@
 package gallifreyc.extension;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import gallifreyc.ast.PostCondition;
 import gallifreyc.ast.PreCondition;
-import polyglot.ast.Formal;
 import polyglot.ast.MethodDecl;
 import polyglot.ast.Node;
-import polyglot.ast.ProcedureDecl;
-import polyglot.ext.jl5.ast.JL5Ext;
-import polyglot.ext.jl5.ast.JL5FormalExt;
-import polyglot.ext.jl5.types.JL5Flags;
-import polyglot.ext.jl5.types.JL5TypeSystem;
-import polyglot.ext.jl5.types.TypeVariable;
-import polyglot.types.Flags;
-import polyglot.types.MethodInstance;
-import polyglot.types.ParsedClassType;
 import polyglot.types.SemanticException;
-import polyglot.types.Type;
-import polyglot.types.TypeSystem;
-import polyglot.types.UnknownType;
 import polyglot.util.SerialVersionUID;
 import polyglot.visit.TypeBuilder;
 
 // extends method declarations to hold an optional pre/post condition, and a flag for whether it's a test method
-public class GallifreyMethodDeclExt extends GallifreyExt {
+public class GallifreyMethodDeclExt extends GallifreyProcedureDeclExt {
     private static final long serialVersionUID = SerialVersionUID.generate();
     
     public PreCondition pre;
